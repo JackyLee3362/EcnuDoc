504cdba24f953474599fc7b4e60d33e2764913e4
Jacky Lee <53095830+JackyLee3362@users.noreply.github.com>
2022-10-19 21:36:57 +0800

    上传测试代码

--- SAT/test_DPLL.py
+++ SAT/test_DPLL.py
@@ -1,9 +1,9 @@
 from sympy import symbols
-from DPLL import ConjunctiveNormalForm, DPLL
+from DPLL import dpll_algorithm, Conjunctive
 
 def test_DPLL():
   a, b, c, d, e = symbols('a b c d e')
-  l = [[~c], [~b], [d, e], [~a], [a, c]]
-  assert DPLL(ConjunctiveNormalForm(l)) == 0 
-  l = [[a,~b], [~a,b]]
-  assert DPLL(ConjunctiveNormalForm)
+  conj = [[~c], [~b], [d, e], [~a], [a, c]]
+  assert dpll_algorithm(Conjunctive(conj)) == 0
+  conj = [[a,~b], [~a,b]]
+  assert dpll_algorithm(Conjunctive(conj)) == 1

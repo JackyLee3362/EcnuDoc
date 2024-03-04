76fe8d2bee633952c438b63a9ecc0b3d5540ecd2
Jackylee <779557618@qq.com>
2024-03-05 00:30:31 +0800

    【清空仓库】

--- SAT/test_DPLL.py
+++
@@ -1,9 +0,0 @@
-from sympy import symbols
-from DPLL import dpll_algorithm, Conjunctive
-
-def test_DPLL():
-  a, b, c, d, e = symbols('a b c d e')
-  conj = [[~c], [~b], [d, e], [~a], [a, c]]
-  assert dpll_algorithm(Conjunctive(conj)) == 0
-  conj = [[a,~b], [~a,b]]
-  assert dpll_algorithm(Conjunctive(conj)) == 1
--- 专业英语-演讲材料/scripts/webp2png.py
+++
@@ -1,6 +0,0 @@
-from PIL import Image
-image = Image.open('./pic/ai2.webp')
-# image.load()
-image.show()
-image.save('./pic/ai2.png')
-print('done')
